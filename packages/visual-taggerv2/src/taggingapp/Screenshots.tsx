fb18e5ca3f382b57c0b3e22a1fa1ef1a929ef694
Nageswara Mannem <nageswara@syftdata.com>
2023-06-09 16:00:06 -0700

    done more polishing

--- packages/visual-taggerv2/src/taggingapp/Screenshots.tsx
+++ packages/visual-taggerv2/src/taggingapp/Screenshots.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Image } from "antd";
 import Section from "../common/components/core/Section";
+import { Css, Flex } from "../common/styles/common.styles";
 
 type ScreenshotProps = {
   screenshot: string;
@@ -9,19 +10,21 @@ const Screenshots = ({ screenshot }: ScreenshotProps) => {
   const [visible, setVisible] = useState(false);
   return (
     <Section title="Screenshot" expandable={true} defaultExpanded={false}>
-      <Image
-        preview={{ visible: false }}
-        width={200}
-        src={screenshot}
-        onClick={() => setVisible(true)}
-      />
-      <div style={{ display: "none" }}>
-        <Image.PreviewGroup
-          preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}
-        >
-          <Image src={screenshot} />
-        </Image.PreviewGroup>
-      </div>
+      <Flex.Col className={Css.padding(8)}>
+        <Image
+          preview={{ visible: false }}
+          width={120}
+          src={screenshot}
+          onClick={() => setVisible(true)}
+        />
+        <div style={{ display: "none" }}>
+          <Image.PreviewGroup
+            preview={{ visible, onVisibleChange: (vis) => setVisible(vis) }}
+          >
+            <Image src={screenshot} />
+          </Image.PreviewGroup>
+        </div>
+      </Flex.Col>
     </Section>
   );
 };
