b9dabe12991a19f7adb485219d400c2c731f342c
Naagas M <125933736+naagas-syft@users.noreply.github.com>
2023-08-30 16:19:53 -0700

    Release 0.1.32 (#67)
    * add custom presets to GA4
    
    * v0.1.32

--- packages/next/src/router/destinations/index.ts
+++ packages/next/src/router/destinations/index.ts
@@ -7,16 +7,41 @@ import mixpanel from '@segment/action-destinations/dist/destinations/mixpanel';
 import slack from '@segment/action-destinations/dist/destinations/slack';
 import ga4 from '@segment/action-destinations/dist/destinations/google-analytics-4';
 
+const CUSTOM_PRESETS = {
+  ga4: [
+    {
+      subscribe: 'type = "track"',
+      partnerAction: 'customEvent',
+      type: 'automatic'
+    },
+    {
+      subscribe: 'type = "page" or type = "screen"',
+      partnerAction: 'pageView',
+      type: 'automatic'
+    },
+    {
+      subscribe: 'type = "identify"',
+      partnerAction: 'login',
+      type: 'automatic'
+    }
+  ]
+};
+
 export const destinations: Record<string, DestinationDefinition> = {};
 function register(name: string, definition: DestinationDefinition<any>): void {
   destinations[name] = definition;
+  if (definition.presets == null) {
+    definition.presets = CUSTOM_PRESETS[name];
+  }
 }
 
 register('amplitude', amplitude);
-register('slack', slack);
 register('heap', heap);
 register('june', june);
 register('mixpanel', mixpanel);
+
+// NO PRESETS
+register('slack', slack);
 register('ga4', ga4);
 
 export function getDestination(key: string): Destination | null {
