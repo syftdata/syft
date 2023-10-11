610f210353a27be89220e7b78542d031859ace40
Nageswara Mannem <nageswara@syftdata.com>
2023-10-10 19:49:04 -0700

    dont use dom-utils

--- packages/next/src/hooks/useFormSubmit.ts
+++ packages/next/src/hooks/useFormSubmit.ts
@@ -1,13 +1,48 @@
 import { useEffect } from 'react';
-import { getAttributeFields, isBrowser } from '../common/utils';
-import { parseUrl } from 'dom-utils';
+import { isBrowser } from '../common/utils';
+
+function getAttributeFields(
+  element: HTMLElement,
+  prefix: string = ''
+): Record<string, unknown> {
+  const attributes = { ...element.dataset };
+  const attributeFields = {};
+
+  Object.keys(attributes).forEach(function (attribute) {
+    // The `on` prefix is used for event handling but isn't a field.
+    if (attribute.indexOf(prefix) === 0) {
+      let value: string | boolean = attributes[attribute];
+
+      // Detects Boolean value strings.
+      if (value === 'true') value = true;
+      if (value === 'false') value = false;
+
+      const field = attribute.slice(prefix.length);
+      attributeFields[field] = value;
+    }
+  });
+
+  return attributeFields;
+}
+
+function parseFormAction(
+  formAction: string,
+  baseUrl: string = window.location.href
+): URL | null {
+  try {
+    const parsedURL = new URL(formAction, baseUrl);
+    return parsedURL;
+  } catch (error) {
+    return null;
+  }
+}
 
 export interface UseFormSubmitOptions {
-  callback: (url: Location, attributes: Record<string, unknown>) => void;
+  callback: (url: URL, attributes: Record<string, unknown>) => void;
 
   enabled?: boolean;
 
-  shouldTrackOutboundForm?: (form: HTMLFormElement, url: Location) => boolean;
+  shouldTrackOutboundForm?: (form: HTMLFormElement, url: URL) => boolean;
 }
 
 export function useFormSubmit({
@@ -17,7 +52,10 @@ export function useFormSubmit({
 }: UseFormSubmitOptions): void {
   const trackSubmit = (event: SubmitEvent): void => {
     const form = event.target as HTMLFormElement;
-    const parsedUrl: Location = parseUrl(form.action);
+    const parsedUrl: URL = parseFormAction(form.action);
+    if (parsedUrl == null) {
+      return;
+    }
     if (shouldTrackOutboundForm != null) {
       const shouldTrack = shouldTrackOutboundForm(form, parsedUrl);
       if (!shouldTrack) {
